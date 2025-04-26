2fe1c93abfa08191fe845ccfa6e1534404198f5d
Your Name <you@example.com>
2025-04-26 21:07:40 +0200

    Auto-commit: Sat Apr 26 21:07:40 CEST 2025

--- src/store.js
+++ src/store.js
@@ -13,7 +13,13 @@ const state = proxy({
   ],
   decals: ['react', 'three2', 'pmndrs'],
   selectedColor: '#EFBD4E',
-  selectedDecal: 'three2'
+  selectedDecal: 'three2',
+  // Custom image upload state
+  customImage: null,
+  isCustomImage: false,
+  customImagePosition: [0, 0.04, 0.15],
+  customImageRotation: [0, 0, 0],
+  customImageScale: 0.15
 })
 
 export { state }
